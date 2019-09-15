4ab895b988bb22727edaf15cf8fdc762ef764a63
oleg <olegjs@users.noreply.github.com>
2019-09-14 19:58:58 -0700

    Test default hash algorithm

--- checksum.test.js
+++ checksum.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert')
 
 const {
+  DEFAULT_ALGORITHM,
   SHA1,
   checksum,
   checksumFilePath,
@@ -11,6 +12,9 @@ const {
 assert(checksum('', SHA1) === 'da39a3ee5e6b4b0d3255bfef95601890afd80709')
 assert(checksum('42', SHA1) === '92cfceb39d57d914ed8b14d0e37643de0797ae56')
 assert(checksum('abc', SHA1) === 'a9993e364706816aba3e25717850c26c9cd0d89d')
+
+assert(DEFAULT_ALGORITHM === SHA1)
+assert(checksum('foo') === checksum('foo', SHA1))
 
 assert(hashFromFileContent('1234 x.txt') === '1234')
 assert(hashFromFileContent('   1234   x.txt    ') === '1234')
