660056c942e71d03c24607ed68b8cd3b69b073b3
Oleg <olegjs@users.noreply.github.com>
2019-11-03 01:11:29 -0800

    Keep the it simple, no more promises

--- src/cli.test.js
+++ src/cli.test.js
@@ -1,15 +1,15 @@
-const { promisify } = require('util')
-const exec = promisify(require('child_process').exec)
+const { execSync } = require('child_process')
 const { ok, ifError, strictEqual: equal } = require('assert')
-const { join } = require('path')
+const path = require('path')
 const { existsSync, unlinkSync } = require('fs')
-const unlink = promisify(require('fs').unlink)
 const { getChecksumFilePath } = require('./checksum')
-const { version } = require('../package.json')
+const package = require('../package.json')
 const { magenta, yellow } = require('chalk')
 
+const run = cmd => execSync(cmd, { encoding: 'utf-8' })
+
 module.exports = () => {
-  const scriptPath = join(__dirname, 'cli.js')
+  const scriptPath = path.join(__dirname, 'cli.js')
   const command = `node ${scriptPath}`
 
   const options = [
@@ -21,38 +21,36 @@ module.exports = () => {
     '--help',
   ]
 
-  exec(`${command} --help`)
-    .then(({ stdout }) => {
-      options.forEach(option =>
-        ok(stdout.includes(option), `Expect "${option}" in help`),
-      )
-    })
-    .catch(ifError)
+  try {
+    const helpText = run(`${command} --help`)
 
-  exec(`${command} --version`)
-    .then(({ stdout }) => equal(stdout.trim(), version))
-    .catch(ifError)
+    options.forEach(option =>
+      ok(helpText.includes(option), `Expect "${option}" in help`),
+    )
 
-  const checksumFilePath = getChecksumFilePath(scriptPath)
-  const payload = 'echo'
+    const versionText = run(`${command} --version`)
+    equal(versionText.trim(), package.version)
 
-  if (existsSync(checksumFilePath)) unlinkSync(checksumFilePath)
+    const checksumFilePath = getChecksumFilePath(scriptPath)
+    const payload = 'echo'
 
-  exec(`${command} --file ${scriptPath} ${payload}`)
-    .then(({ stdout }) => {
-      ok(stdout.includes(payload))
-      ok(stdout.includes(scriptPath))
-      return exec(`${command} -f ${scriptPath} ${payload}`)
-    })
-    .then(({ stdout }) => {
-      equal(stdout, '')
-      return unlink(checksumFilePath)
-    })
-    .then(() => exec(`${command} --color --file ${scriptPath} ${payload}`))
-    .then(({ stdout }) => {
-      ok(stdout.includes(magenta(scriptPath)))
-      ok(stdout.includes(yellow(payload)))
-      return unlink(checksumFilePath)
-    })
-    .catch(ifError)
+    if (existsSync(checksumFilePath)) unlinkSync(checksumFilePath)
+
+    const firstRunText = run(`${command} --file ${scriptPath} ${payload}`)
+    ok(firstRunText.includes(payload))
+    ok(firstRunText.includes(scriptPath))
+
+    const secondRunText = run(`${command} -f ${scriptPath} ${payload}`)
+    equal(secondRunText, '')
+
+    unlinkSync(checksumFilePath)
+
+    const colorText = run(`${command} --color --file ${scriptPath} ${payload}`)
+    ok(colorText.includes(magenta(scriptPath)))
+    ok(colorText.includes(yellow(payload)))
+
+    unlinkSync(checksumFilePath)
+  } catch (error) {
+    ifError(error)
+  }
 }
