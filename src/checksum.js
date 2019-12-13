0e3e25281a9819e140eb90710736f544512d9d25
Oleg <olegjs@users.noreply.github.com>
2019-12-12 17:38:55 -0800

    Refactor "checksum.js"

--- src/checksum.js
+++ src/checksum.js
@@ -1,24 +1,19 @@
-const {
-  basename,
-  dirname,
-  format: formatPath,
-  join: joinPath,
-} = require('path')
-
+const path = require('path')
+const { basename, dirname } = path
 const { createHash } = require('crypto')
-const { algorithm: default_algorithm, fileExtension } = require('./defaults')
+const { algorithm: defaultAlgorithm, fileExtension } = require('./defaults')
 
-const getChecksum = (s, algorithm = default_algorithm, format = 'hex') =>
+const getChecksum = (s, algorithm = defaultAlgorithm, format = 'hex') =>
   createHash(algorithm)
     .update(s)
     .digest(format)
 
 const hashFromFileContent = s => s.trim().split(/\s+/)[0]
 
-const getChecksumFilePath = path =>
-  joinPath(
-    dirname(path),
-    formatPath({ name: `.${basename(path)}`, ext: fileExtension }),
+const getChecksumFilePath = filePath =>
+  path.join(
+    dirname(filePath),
+    path.format({ name: `.${basename(filePath)}`, ext: fileExtension }),
   )
 
 module.exports = { getChecksum, getChecksumFilePath, hashFromFileContent }
